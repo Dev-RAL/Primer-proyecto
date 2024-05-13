1cc4f0fd66e703e5ddab6bcce90184d0491c814d
Dev-RAL <ylas_456@hotmail.com>
2024-05-13 09:57:13 -0500

    se agrego las libreuia iomanip y conio

--- Historial_Medico.cpp
+++ Historial_Medico.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <iomanip>
+#include <conio.h>
 using namespace std;
 int main (){
     int i;
     printf("Inicio de historial medico");
+    getch();
     return 0;
 }
